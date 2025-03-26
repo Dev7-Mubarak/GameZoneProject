7930bf35bc68446850d7985cd8d4a452c8512aec
OsKhaled9 <151741177+OsKhaled9@users.noreply.github.com>
2025-03-26 21:57:22 +0300

    Adding Games Categories functionality
    adding bootbox library to project

---
+++ GameZone/Areas/Admin/ViewModels/GamesCategoriesVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameZone.Areas.Admin.ViewModels
+{
+    public class GamesCategoriesVM
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter category name!.")]
+        [MaxLength(15, ErrorMessage = "Maximum length of characters is 15")]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; } = null!;
+    }
+}
