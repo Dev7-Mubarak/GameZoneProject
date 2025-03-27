6ea0dc2968419e657f3704bcbdb9d4e7a8845f37
Mubarak <mubark7382@gmail.com>
2025-03-27 15:58:53 +0300

    Add User Management Functionality

---
+++ GameZone/Areas/Admin/ViewModels/EditUserVM.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameZone.Areas.Admin.ViewModels
+{
+    public class EditUserVM
+    {
+        public string Id { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string RoleId { get; set; }
+        public IEnumerable<SelectListItem> Roles { get; set; } = new List<SelectListItem>();
+    }
+}
---
+++ GameZone/Areas/Admin/ViewModels/UserVM.cs
@@ -0,0 +1,10 @@
+﻿namespace GameZone.Areas.Admin.ViewModels
+{
+    public class UserVM
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Emile { get; set; }
+        public string Role { get; set; }
+    }
+}
