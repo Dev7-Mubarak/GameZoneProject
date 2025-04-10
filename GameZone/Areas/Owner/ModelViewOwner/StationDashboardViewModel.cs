9bd5ea482624901b8258124f5f585f3adf33da87
AmmarBawazirTech <AmmarBawazirTech@gmail.com>
2025-04-10 21:20:33 +0300

    Fix the bugs in the buttons action add the filter by status apply the statictics in the owner view

---
+++ GameZone/Areas/Owner/ModelViewOwner/StationDashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GameZone.Models;
+
+namespace GameZone.Areas.Owner.ModelViewOwner
+{
+    public class StationDashboardViewModel
+    {
+        public GameStation Station { get; set; }
+        public int PendingCount { get; set; }
+        public int CompletedCount { get; set; }
+        public int DeniedCount { get; set; }
+    }
+}
